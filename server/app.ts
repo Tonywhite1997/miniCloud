272773e7272929a3ba4fe90280e8dd16fe58b883
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-02-22 12:01:33 +0100

    undo the last commit

--- server/app.ts
+++ server/app.ts
@@ -15,8 +15,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    // origin: "http://localhost:5173",
-    origin: "https://minicloud.onrender.com",
+    origin: "http://localhost:5173",
+    // origin: "https://minicloud.onrender.com",
   })
 );
 
