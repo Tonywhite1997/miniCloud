fdab86027ae03e7ee8e078e31e79aa110728ef6e
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-02-23 12:12:43 +0100

    changed back the url

--- client/src/utils/generalCommands/ReturnToLoginPage.ts
+++ client/src/utils/generalCommands/ReturnToLoginPage.ts
@@ -1,10 +1,8 @@
-import urls from "../authURL";
-
 export function returnToLoginPage(error) {
   if (
     error?.response?.data?.message?.includes("Token expired") ||
     error?.response?.data?.message?.includes("invalid token")
   ) {
-    window.location.assign(`${urls.authURL}/login`);
+    window.location.assign("/auth/login");
   }
 }
