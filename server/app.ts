366a70a67252383b75c1fe7a2bc5fe2950d74ca5
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-02-23 11:49:11 +0100

    fixed the hosted fromtend url in cors

--- server/app.ts
+++ server/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:5173",
-    origin: "https://miniycloud.onrender.com/",
+    origin: "https://miniycloud.onrender.com",
   })
 );
 
