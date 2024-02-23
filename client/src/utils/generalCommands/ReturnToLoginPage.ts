c28270a34fb933376d72ccba6e8e7e021ff875de
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-02-23 12:06:47 +0100

    fixing some urls in the client files

--- client/src/utils/generalCommands/ReturnToLoginPage.ts
+++ client/src/utils/generalCommands/ReturnToLoginPage.ts
@@ -1,8 +1,10 @@
+import urls from "../authURL";
+
 export function returnToLoginPage(error) {
   if (
     error?.response?.data?.message?.includes("Token expired") ||
     error?.response?.data?.message?.includes("invalid token")
   ) {
-    window.location.assign("/auth/login");
+    window.location.assign(`${urls.authURL}/login`);
   }
 }
